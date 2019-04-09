4cc21abecfac87bc98998622a855c228e23e5df1
Maxim-Turovets <mturove71@gmail.com>
2019-04-09 14:25:11 +0300

    addMessage class

--- app/src/main/java/com/example/mtchat_android/chatActivity.java
+++ app/src/main/java/com/example/mtchat_android/chatActivity.java
@@ -1,4 +1,38 @@
 package com.example.mtchat_android;
 
-public class chatActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class chatActivity extends AppCompatActivity {
+
+
+
+    private String channelID = "CHANNEL_ID_FROM_YOUR_SCALEDRONE_DASHBOARD";
+    private String roomName = "observable-room";
+    private EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // This is where we write the mesage
+        editText = (EditText) findViewById(R.id.editText);
+    }
+
+
+
+    public void sendMessage(View view) {
+        String message = editText.getText().toString();
+        if (message.length() > 0) {
+
+            editText.getText().clear();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/mtchat_android/chatTypeActivity.java
+++ app/src/main/java/com/example/mtchat_android/chatTypeActivity.java
@@ -1,8 +1,10 @@
 package com.example.mtchat_android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class chatTypeActivity extends AppCompatActivity {
     @Override
@@ -11,4 +13,10 @@ public class chatTypeActivity extends AppCompatActivity {
         setContentView(R.layout.chat_type_layout);
     }
 
+
+    public  void goToChat(View view)
+    {
+        Intent intent = new Intent(this,chatActivity.class);
+        startActivity(intent);
+    }
 }
