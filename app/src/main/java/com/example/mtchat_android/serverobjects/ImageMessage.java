c032b53a9beafe741fe55b9e816bc7acc0c1ed8b
Maxim-Turovets <mturove71@gmail.com>
2019-04-22 22:41:07 +0300

    set and get method

--- app/src/main/java/com/example/mtchat_android/serverobjects/ImageMessage.java
+++ app/src/main/java/com/example/mtchat_android/serverobjects/ImageMessage.java
@@ -1,4 +1,22 @@
 package com.example.mtchat_android.serverobjects;
 
 public class ImageMessage {
+    private String objectType = "ImageMessage";
+    private boolean  available;
+
+    public String getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(String objectType) {
+        this.objectType = objectType;
+    }
+
+    public boolean isAvailableж() {
+        return available;
+    }
+
+    public void setAvailableж(boolean availableж) {
+        this.available = available;
+    }
 }
