0dcffe9b7e9748ac708fd3ecd87167ce216dcbf2
Maxim-Turovets <mturove71@gmail.com>
2019-05-16 04:35:15 +0300

    add setting button and greeting

--- app/src/main/java/com/example/mtchat_android/activitys/MainActivity.java
+++ app/src/main/java/com/example/mtchat_android/activitys/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mtchat_android.activitys;
 
 import android.content.Intent;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,13 +17,27 @@ public class  MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.greeting_layout);
 
+        new CountDownTimer(1_500,1220) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+            @Override
+            public void onFinish() {
+                nextLayout();
+            }
+        }.start();
+
+
+
+    }
+
+    public  void  nextLayout()
+    {
         this.finish();
         Intent intent = new Intent(this, ChatTypeActivity.class);
         startActivity(intent);
-
-
     }
 
 
